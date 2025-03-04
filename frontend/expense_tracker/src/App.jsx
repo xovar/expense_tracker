592d7d3a884d6e3463f7f91322d41f4cd30028be
FARHAN X0VAR <farhanx0var@FARHANs-MacBook-Air.local>
2025-03-04 17:53:54 +0600

    directories added

--- frontend/expense_tracker/src/App.jsx
+++ frontend/expense_tracker/src/App.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import Login from './pages/auth/Login';
+import SignUp from './pages/auth/SignUp';
+import Home from './pages/Dashboard/Home';
+import Income from './pages/Dashboard/Income';
+import Expense from './pages/Dashboard/Expense';
 
 function App() {
   return (
-    <div className="text-2xl border-l-8">
-      App
+    <div>
+      <Router>
+          <Routes>
+              <Route path='/' element={<Root/>}/>
+              <Route path='/login' exact element={<Login />}/>
+              <Route path='/signUp' exact element={<SignUp />}/>
+              <Route path='/dashboard' exact element={<Home />}/>
+              <Route path='/income' exact element={<Income />}/>
+              <Route path='/expense' exact element={<Expense />}/>
+          </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+
+const Root = () => {
+  // check if token exists in localstorage
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  // Redirected to the dashboard if Authenticated Otherwise to Login
+
+  return isAuthenticated ? (
+    <Navigate to="/dashboard"/>
+  ) : (
+    <Navigate to="/login" />
+  )
+}
